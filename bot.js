ec0966e2f3dda8b9636294c1ff135df11742278e
Julie Pham <julie.pham@uconn.edu>
2017-06-28 19:15:24 -0400

    Add Status Interval

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-console.log('The bot is starting');
+console.log('Beep boop beep, Bot activated.');
 
 //import the package/library twit and config
 var Twit = require('twit');
@@ -6,22 +6,32 @@ var config = require('./config');
 var T = new Twit(config);
 var params = {
   q: 'ice cream',
-  count: 30
-}
-var tweet = {
-  status: 'hello world!'
+  count: 2
 }
 
-T.post('statuses/update', tweet, tweeted);
+setInterval(tweetIt, 1000*20)
 
-function tweeted(err, data, response) {
- console.log(data)
-};
+function tweetIt() {
+  var r = Math.floor(Math.random()*100);
+  var tweet = {
+    status: 'beep boop, Bot reporting..here is a random number ' + r + ' from node.js :)'
+  }
+
+  T.post('statuses/update', tweet, tweeted);
+
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log("Error");
+    }
+   else {
+     console.log("It worked");
+   }
+  }
+}
 
 T.get('search/tweets', params, gotData);
 
 function gotData(err, data, response) {
-  console.log(data);
   var tweets = data.statuses;
   for(var i = 0; i < tweets.length; i++) {
     console.log(tweets[i].text);
